6c8eeecba781d0f04cdaf1b484ed8e821d14e9cb
Lee Kelly <leeowenkelly@gmail.com>
2023-03-09 15:17:31 +0000

    core exercises completed

--- src/core.js
+++ src/core.js
@@ -1,4 +1,9 @@
 // 1. Create a function named bakingTime that returns 50
+function bakingTime() {
+  return 50
+}
+
+console.log(bakingTime())
 
 // 2. Create a function named remainingBakeTime that accepts one parameter:
 // - the number of minutes your cake has been in the oven.
@@ -6,11 +11,23 @@
 // It must return how many minutes your cake has left based on your parameter
 // and the result of the bakingTime function
 
+function remainingBakeTime(timeInOven) {
+  return bakingTime() - timeInOven
+}
+
+console.log(remainingBakeTime(10))
+
 // 3. Create a function named calculatePreparationTime that accepts one parameter:
 // - the number of layers your cake has
 //
 // It must return how many minutes it will take to prepare your cake, based on
 // each layer taking 3 minutes to prepare
+
+function calculatePreparationTime(numOfLayers) {
+  return numOfLayers * 3
+}
+
+console.log(calculatePreparationTime(4))
 
 // 4. Create a function named totalTimeSpent that accepts two parameters:
 // - the number of layers your cake has
@@ -18,6 +35,12 @@
 // It must return how many minutes in total you have spent making the cake,
 // which is the sum of the preparation time and the number of minutes it's been in the oven.
 // Use your calculatePreparationTime function in the calculation.
+
+function totalTimeSpent(numOfLayers, timeInOven) {
+  return calculatePreparationTime(numOfLayers) + timeInOven
+}
+
+console.log(totalTimeSpent(4, 10))
 
 // Don't change the code below this line
 module.exports = {
