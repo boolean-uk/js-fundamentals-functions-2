5c3205760f717f6e811c9ff0d9f5dadb87ec54ee
John Poikilidis <akiskawi@gmail.com>
2023-04-11 18:05:15 +0300

    First Commit

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,7 +4,15 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
-
+function timerStatus(remainingTime) {
+  if (remainingTime === 0) {
+    return "Phil's cake is ready!"
+  } else if (remainingTime > 0) {
+    return 'The cake is still baking!'
+  } else {
+    return "You didn't set a timer!"
+  }
+}
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,6 +21,10 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+
+function estimatePrepTime(ingredients, prepTime = 2) {
+  return ingredients.length * prepTime
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -29,6 +41,16 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
+function calculateQuantities(ingredients, layers) {
+  const returnObj = {}
+  ingredients.includes('sugar')
+    ? (returnObj.sugar = 100 * layers)
+    : (returnObj.sugar = 0)
+  ingredients.includes('eggs')
+    ? (returnObj.eggs = 2 * layers)
+    : (returnObj.eggs = 0)
+  return returnObj
+}
 
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
@@ -42,7 +64,12 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
-
+function improveRecipe(obj, portion) {
+  for (const key in obj) {
+    obj[key] = obj[key] * portion
+  }
+  return obj
+}
 // Don't change the code below this line
 module.exports = {
   timerStatus /* eslint-disable-line no-undef */,
