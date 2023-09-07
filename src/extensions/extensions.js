3841790b968e8229425b46bc256f86fd9f202e03
Kye <NovusAa12@proton.me>
2023-09-07 22:01:39 +0100

    completed core task & extensions

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,7 +4,15 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
-
+const timerStatus = (minsLeft) => {
+  if (minsLeft === 0) {
+    return "Phil's cake is ready!"
+  } else if (minsLeft > 0) {
+    return 'The cake is still baking!'
+  } else {
+    return "You didn't set a timer!"
+  }
+}
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,6 +21,14 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+const estimatePrepTime = (arr, prepTime) => {
+  const ingredients = []
+  if (!prepTime) {
+    prepTime = 2
+  }
+  ingredients.push(arr)
+  return arr.length * prepTime
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -29,6 +45,17 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
+const calculateQuantities = (arr, layers) => {
+  const list = { sugar: 0, eggs: 0 }
+  const ingredients = arr.map((item) => item.toLowerCase())
+  if (ingredients.indexOf('sugar') >= 0) {
+    list.sugar = 100 * layers
+  }
+  if (ingredients.indexOf('eggs') >= 0) {
+    list.eggs = 2 * layers
+  }
+  return list
+}
 
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
@@ -42,7 +69,10 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
-
+const improveRecipe = (obj, portions) => {
+  Object.keys(obj).forEach((key) => (obj[key] *= portions))
+  return obj
+}
 // Don't change the code below this line
 module.exports = {
   timerStatus /* eslint-disable-line no-undef */,
