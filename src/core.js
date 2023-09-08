306334b0ff6cef39ce15bdb028d286fc0657f365
Usama <usamaibrahimusman@gmail.com>
2023-09-08 16:15:14 +0100

    exercise 3

--- src/core.js
+++ src/core.js
@@ -18,10 +18,15 @@ console.log(remainingBakeTime(10))
 
 // 3. Create a function named calculatePreparationTime that accepts one parameter:
 // - the number of layers your cake has
-//
+
 // It must return how many minutes it will take to prepare your cake, based on
 // each layer taking 3 minutes to prepare
+function calculatePreparationTime(number_of_layers) {
+   let take_to_take =  number_of_layers * 3
+   return take_to_take
+}
 
+console.log(calculatePreparationTime(2))
 // 4. Create a function named totalTimeSpent that accepts two parameters:
 // - the number of layers your cake has
 // - the number of minutes the cake has already been baking in the oven
