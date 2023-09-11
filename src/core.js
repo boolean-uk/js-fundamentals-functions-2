7071a80144a8c412de359f633fad08ffd3a782f3
lokismelc <elviscu90@gmail.com>
2023-09-11 17:22:04 +0100

    working on it

--- src/core.js
+++ src/core.js
@@ -1,17 +1,28 @@
 // 1. Create a function named bakingTime that returns 50
 
+function bakingTime() {
+  return 50
+}
 // 2. Create a function named remainingBakeTime that accepts one parameter:
 // - the number of minutes your cake has been in the oven.
-//
+function remainingBakeTime(minutesInOven) {
+  const totalBakingTime = bakingTime()
+  const remanig = totalBakingTime - minutesInOven
+  return remanig
+}
+console.log(remainingBakeTime(40))
 // It must return how many minutes your cake has left based on your parameter
 // and the result of the bakingTime function
 
 // 3. Create a function named calculatePreparationTime that accepts one parameter:
 // - the number of layers your cake has
-//
+function calculatePreparationTime() {
+  const cakeLayers = (3)
+}
+
 // It must return how many minutes it will take to prepare your cake, based on
 // each layer taking 3 minutes to prepare
-
+function 
 // 4. Create a function named totalTimeSpent that accepts two parameters:
 // - the number of layers your cake has
 // - the number of minutes the cake has already been baking in the oven
@@ -22,7 +33,7 @@
 // Don't change the code below this line
 module.exports = {
   bakingTime /* eslint-disable-line no-undef */,
-  remainingBakeTime /* eslint-disable-line no-undef */,
-  calculatePreparationTime /* eslint-disable-line no-undef */,
-  totalTimeSpent /* eslint-disable-line no-undef */
+  remainingBakeTime /* eslint-disable-line no-undef */
+  // calculatePreparationTime /* eslint-disable-line no-undef */,
+  // totalTimeSpent /* eslint-disable-line no-undef */
 }
