ffb18793f4ba9877ddf57241af9013f5b709bba7
kristianverduin <kristianverduin@gmail.com>
2024-02-19 09:38:28 +0100

    Finished everything

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -5,6 +5,17 @@
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
 
+function timerStatus(min) {
+  if (min === undefined) {
+    return "You didn't set a timer!"
+  }
+  if (min === 0) {
+    return "Phil's cake is ready!"
+  } else {
+    return 'The cake is still baking!'
+  }
+}
+
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,6 +24,13 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+
+function estimatePrepTime(ingredients, prepTime) {
+  if (prepTime === undefined) {
+    prepTime = 2
+  }
+  return prepTime * ingredients.length
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -30,6 +48,25 @@
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
 
+function calculateQuantities(ingredients, layers) {
+  const object = {
+    sugar: 0,
+    eggs: 0
+  }
+
+  for (let i = 0; i < ingredients.length; i++) {
+    if (ingredients[i] === 'sugar') {
+      object.sugar += 100
+    } else if (ingredients[i] === 'eggs') {
+      object.eggs += 2
+    }
+  }
+
+  object.sugar *= layers
+  object.eggs *= layers
+  return object
+}
+
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,6 +79,16 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
+
+function improveRecipe(ingredients, portions) {
+  const newRecipe = {}
+  for (const ingredient in ingredients) {
+    newRecipe[ingredient] = ingredients[ingredient] * portions
+  }
+  return newRecipe
+}
+
+improveRecipe({ sugar: 200, eggs: 2, milk: 100 }, 2)
 
 // Don't change the code below this line
 module.exports = {
