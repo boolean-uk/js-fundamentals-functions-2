7c789c3765f6099abe583dcb1246a3627c55fe7b
Mateusz Pietrzak <matti3301@gmail.com>
2024-02-20 09:18:49 +0100

    Completed al tasks

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -5,6 +5,16 @@
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
 
+function timerStatus(timer) {
+  if (timer) {
+    return 'The cake is still baking!'
+  } else if (timer === 0) {
+    return `Phil's cake is ready!`
+  } else {
+    return `You didn't set a timer!`
+  }
+}
+
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,6 +23,9 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+function estimatePrepTime(ingredients, prepTime = 2) {
+  return ingredients.length * prepTime
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -30,6 +43,12 @@
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
 
+function calculateQuantities(ingredients, layers) {
+  const sugar = ingredients.includes('sugar') ? layers * 100 : 0
+  const eggs = ingredients.includes('eggs') ? layers * 2 : 0
+  return { sugar, eggs }
+}
+
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,6 +61,14 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
+
+function improveRecipe(ingredients, portions) {
+  const improvedRecipe = {}
+  for (const ingredient in ingredients) {
+    improvedRecipe[ingredient] = ingredients[ingredient] * portions
+  }
+  return improvedRecipe
+}
 
 // Don't change the code below this line
 module.exports = {
