01abb7e24ca3cd96564c6e6303df0af399188f48
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-02-20 11:14:02 +0100

    Tests: Passed All

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -78,9 +78,24 @@ function parseRequest(req) {
     body: null,
     query: null
   }
-
-  // call the other functions below as needed
-
+  req = splitIntoLines(req)
+  if (req.length === 0) return request
+  const line1 = req[0].trim().split(' ')
+  request.method = line1[0]
+  const path = line1[1]
+  request.query = extractQuery(path)
+  request.path =
+    request.query === null ? path : path.substring(0, path.indexOf('?'))
+  if (req.length > 1) {
+    for (let i = 1; i < req.length; i++) {
+      if (req[i].trim()[0] === '{') {
+        const bodyStr = req.slice(i).join('')
+        request.body = parseBody(bodyStr)
+        break
+      }
+      parseHeader(req[i], request.headers)
+    }
+  }
   return request
 }
 
@@ -92,7 +107,12 @@ function parseRequest(req) {
 // eg: parseHeader('Authorization: Bearer your_access_token', { Host: 'www.example.com' })
 //        => { Host: 'www.example.com', Authorization: 'Bearer your_access_token'}
 // eg: parseHeader('', { Host: 'www.example.com' }) => { Host: 'www.example.com' }
-function parseHeader(header, headers) {}
+function parseHeader(header, headers) {
+  const headerParts = header.split(':')
+  if (headerParts.length > 1) {
+    headers[headerParts[0].trim()] = headerParts[1].trim()
+  }
+}
 
 // 3. Create a function named parseBody that accepts one parameter:
 // - a string for the body
@@ -100,14 +120,41 @@ function parseHeader(header, headers) {}
 // search for JSON parsing
 // eg: parseBody('{"key1": "value1", "key2": "value2"}') => { key1: 'value1', key2: 'value2' }
 // eg: parseBody('') => null
-function parseBody(body) {}
+function parseBody(body) {
+  if (body.length < 3) {
+    return null
+  }
+  return JSON.parse(body)
+}
 
 // 4. Create a function named extractQuery that accepts one parameter:
 // - a string for the full path
 // It must return the parsed query as a JavaScript object or null if no query ? is present
 // eg: extractQuery('/api/data/123?someValue=example') => { someValue: 'example' }
 // eg: extractQuery('/api/data/123') => null
-function extractQuery(path) {}
+function extractQuery(path) {
+  if (!path.includes('?')) return null
+  const result = {}
+  let queries = path.split('?')
+  queries = queries[1].includes('&') ? queries[1].split('&') : [queries[1]]
+  for (let i = 0; i < queries.length; i++) {
+    if (queries[i].includes('=')) {
+      const query = queries[i].split('=')
+      result[query[0]] = query[1]
+    }
+  }
+  return result
+}
+
+function splitIntoLines(req) {
+  req = req.split('\n')
+  for (let i = 0; i < req.length; i++) {
+    if (req[i].length < 3) {
+      req.splice(i, 1)
+    }
+  }
+  return req
+}
 
 module.exports = {
   rawGETRequest,
