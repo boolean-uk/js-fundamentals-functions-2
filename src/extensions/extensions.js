6f012adeb5f6b994100e1735b968868f099ab7e1
Angus Townsley <angustownsley@gmail.com>
2024-02-21 16:45:59 +0000

    all exercises complete

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,7 +4,14 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
-
+function timerStatus(timer) {
+  if (typeof timer !== 'number') {
+    return "You didn't set a timer!"
+  } else if (timer === 0) {
+    return "Phil's cake is ready!"
+  }
+  return 'The cake is still baking!'
+}
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,7 +20,16 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
-
+function estimatePrepTime(arr, mins) {
+  if (isNaN(mins)) {
+    mins = 2
+  }
+  let output = 0
+  for (let i = 0; i < arr.length; i++) {
+    output += mins
+  }
+  return output
+}
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
 // - a list of ingredients
@@ -29,7 +45,20 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
-
+function calculateQuantities(ingredientArr, layers) {
+  const output = {}
+  if (ingredientArr.includes('sugar')) {
+    output.sugar = 100 * layers
+  } else {
+    output.sugar = 0
+  }
+  if (ingredientArr.includes('eggs')) {
+    output.eggs = 2 * layers
+  } else {
+    output.eggs = 0
+  }
+  return output
+}
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,7 +71,13 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
-
+function improveRecipe(ingredientObj, servings) {
+  const outputObj = Object.create(ingredientObj)
+  for (const prop in ingredientObj) {
+    outputObj[prop] = ingredientObj[prop] * servings
+  }
+  return outputObj
+}
 // Don't change the code below this line
 module.exports = {
   timerStatus /* eslint-disable-line no-undef */,
