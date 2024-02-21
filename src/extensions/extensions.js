3ad3bfe833c05f0cd214357fc1961d8312da8e3d
Eddy1108 <edvardh@hotmail.no>
2024-02-21 09:21:26 +0100

    Core + Extension

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,6 +4,11 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
+function timerStatus(minutes){
+  if (minutes === undefined) return "You didn't set a timer!"
+  if (minutes <= 0) return "Phil's cake is ready!"
+  if (minutes > 0) return "The cake is still baking!"
+}
 
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
@@ -13,6 +18,13 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+function estimatePrepTime(ingredients, timePer){
+  let timePrep
+  if(timePer === undefined) timePrep = 2
+  else timePrep = timePer
+
+  return ingredients.length * timePrep
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -30,6 +42,14 @@
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
 
+function calculateQuantities(ingredients, layers){
+  let obj = {sugar: 0, eggs: 0}
+  if (ingredients.includes("sugar")) obj.sugar = 100 * layers
+  if (ingredients.includes("eggs")) obj.eggs = 2 * layers
+
+  return obj
+}
+
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,6 +62,19 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
+
+function improveRecipe(ingredients, portions)
+{
+
+  let obj = ingredients
+  if (Object.hasOwn(obj, "eggs")) obj.eggs = obj.eggs * portions
+  if (Object.hasOwn(obj, "milk")) obj.milk = obj.milk * portions
+  if (Object.hasOwn(obj, "sugar")) obj.sugar = obj.sugar * portions
+  if (Object.hasOwn(obj, "flour")) obj.flour = obj.flour * portions
+
+  return obj
+}
+
 
 // Don't change the code below this line
 module.exports = {
