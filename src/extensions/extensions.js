01cf8684782b9bcaeda256d5247532f95e70f6fc
Mads Gjengaar <mig.gjengaar@gmail.com>
2024-02-22 11:54:12 +0100

    Completed All Tasks

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -5,6 +5,12 @@
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
 
+function timerStatus(remainingMins) {
+  if (typeof(remainingMins) !== 'number')
+    return 'You didn\'t set a timer!'
+  return remainingMins <= 0 ? 'Phil\'s cake is ready!' : 'The cake is still baking!'
+}
+
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,6 +19,11 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+
+function estimatePrepTime(ingredients, prepTime) {
+  prepTime = typeof(prepTime) !== 'number' ? 2 : prepTime
+  return ingredients.length * prepTime
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -30,6 +41,19 @@
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
 
+function calculateQuantities(ingredients, layers) {
+  const _outObj = {
+    sugar:0,
+    eggs:0
+  }
+
+  for (var elm of ingredients)
+    if (_outObj[elm] !== undefined)
+      _outObj[elm] = (elm === 'sugar' ? 100 : 2) * layers
+
+  return _outObj
+}
+
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,6 +66,17 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
+
+function improveRecipe(keyIngredients, portions) {
+  const _outObj = {...keyIngredients}
+
+  for (const [key, elm] of Object.entries(_outObj)) {
+    if (typeof(_outObj[key]) !== 'number') continue
+    _outObj[key] = elm * portions
+  }
+
+  return _outObj
+}
 
 // Don't change the code below this line
 module.exports = {
