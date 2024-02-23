fb9c993012d412f6346cb6bcf5995b50993ce276
sebHanssen <sebastian.hanssen@outlook.com>
2024-02-23 11:59:10 +0100

    finish exercise

--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -4,6 +4,15 @@
 // The function must return "Phil's cake is ready!" if the remaining minutes is 0,
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
+function timerStatus(remainingTime) {
+  if (remainingTime === 0) {
+    return "Phil's cake is ready!"
+  } else if (remainingTime > 0) {
+    return 'The cake is still baking!'
+  } else {
+    return "You didn't set a timer!"
+  }
+}
 
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
@@ -13,6 +22,12 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+function estimatePrepTime(ingredients, prepTime) {
+  if (prepTime === undefined) {
+    prepTime = 2
+  }
+  return ingredients.length * prepTime
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -29,6 +44,16 @@
 //
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
+function calculateQuantities(ingredients, cakeLayers) {
+  const result = { sugar: 0, eggs: 0 }
+  if (ingredients.includes('sugar')) {
+    result.sugar = 100 * cakeLayers
+  }
+  if (ingredients.includes('eggs')) {
+    result.eggs = 2 * cakeLayers
+  }
+  return result
+}
 
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
@@ -42,6 +67,13 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
+function improveRecipe(ingredients, portions) {
+  Object.entries(ingredients).forEach((ent) => {
+    const [key, value] = ent
+    ingredients[key] = value * portions
+  })
+  return ingredients
+}
 
 // Don't change the code below this line
 module.exports = {
